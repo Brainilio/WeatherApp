25f1f7d9b042d3b71c15e9d4ed91ede3a3cd6f5f
B.A. Imambaks Rodrigues <0924777@hr.nl>
2018-06-14 21:34:22 +0200

    added weersite

---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+  <div>
+    <h1 className="title-container__title">Weer zoeker</h1>
+    <h3 className="title-container__subtitle">door Brainilio Imambaks</h3>
+  </div>
+);
+
+export default Titles;
